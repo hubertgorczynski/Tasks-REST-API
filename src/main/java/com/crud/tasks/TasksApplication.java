5601acacfa12a4b1396ffcb3cfee5031252782f6
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-06-04 21:03:24 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,14 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication extends SpringBootServletInitializer { - committed for Heroku
+public class TasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
 
-    @Override
+/*  @Override - committed for Heroku
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TasksApplication.class);
     }
+ */
 
 }
