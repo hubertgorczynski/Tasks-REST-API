c04ae0f580e6f11e58901c76bbcb9ad862cadaac
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-08-15 11:49:27 +0200

    Added @NoArgsConstructor annotation in class TaskDto

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,7 +2,9 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 @AllArgsConstructor
 public class TaskDto {
