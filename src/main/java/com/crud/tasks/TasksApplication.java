71cea3547bf2b028ab9c3977e889191c3469efec
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-08-25 20:16:31 +0200

    Changed URL adress for TaskCOntroller

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,22 +2,10 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-
-//public class TasksApplication extends SpringBootServletInitializer - committed for Heroku
 
 @SpringBootApplication
 public class TasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-
-    /* - committed for Heroku
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TasksApplication.class);
-    }
-     */
 }
